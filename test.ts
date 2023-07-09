8d867755c468afd8876c04cf52933093df53bba4
Chabib Nurochman <chabib.nr@gmail.com>
2023-07-10 02:18:20 +0700

    chore: cleanup

--- test.ts
+++
@@ -1,8 +0,0 @@
-import { useObservableFn } from './dist/observable'
-
-const [submit, submission] = useObservableFn(async (foo: number, bar: string) => {
-  return { foo, bar }
-})
-
-submission.result.bar
-submission.error.bar
