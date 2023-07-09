a2e5cddbc327572b2d3a32e89495e5cccaf2dbfa
Chabib Nurochman <chabib.nr@gmail.com>
2023-07-10 02:47:16 +0700

    chore: clean up

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ const [submit, submission] = useObservableFn(
 </script>
 
 <template>
-  <button type="button" @click="submit(1, 'hello')">submit</button>
+  <button type="button" @click="() => submit(1, 'hello')">submit</button>
   <div v-if="submission.isPending">loading</div>
   <div v-if="submission.isFulfilled">{{ submission.result }}</div>
   <div v-if="submission.isRejected">{{ submission.error }}</div>
